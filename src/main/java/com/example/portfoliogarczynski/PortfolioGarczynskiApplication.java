39ddf076bafc6cff2c084d705d6eeacf79ffa4f1
garczynskiDominik <garczynski.dominik@gmail.com>
2021-06-16 22:46:28 +0200

    Initial commit

---
+++ src/main/java/com/example/portfoliogarczynski/PortfolioGarczynskiApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfoliogarczynski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioGarczynskiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioGarczynskiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portfoliogarczynski/PortfolioGarczynskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfoliogarczynski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioGarczynskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
