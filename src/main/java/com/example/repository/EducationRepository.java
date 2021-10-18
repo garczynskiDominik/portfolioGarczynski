f357849986c455994e5c93a95b8c908defff91e5
garczynskiDominik <garczynski.dominik@gmail.com>
2021-10-18 08:45:40 +0200

    Fixed repository, delete imports

--- src/main/java/com/example/repository/EducationRepository.java
+++ src/main/java/com/example/repository/EducationRepository.java
@@ -4,7 +4,6 @@ import com.example.model.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface EducationRepository extends JpaRepository<Education, Long> {
 }
--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.example.repository;
 
 import com.example.model.Users;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, Long> {
 }
 
