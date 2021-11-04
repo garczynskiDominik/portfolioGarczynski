3d1d53093c765f697e3282cca41bd1b8658134fd
garczynskiDominik <garczynski.dominik@gmail.com>
2021-11-04 20:17:33 +0100

    start test controller

---
+++ src/test/java/com/example/contoller/AboutControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.contoller;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AboutControllerTest {
+
+}
