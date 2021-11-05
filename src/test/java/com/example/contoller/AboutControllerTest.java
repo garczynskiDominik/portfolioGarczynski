25d204edae6220deae47592c089f3f573b430467
garczynskiDominik <garczynski.dominik@gmail.com>
2021-11-05 14:00:01 +0100

    start test controller

--- src/test/java/com/example/contoller/AboutControllerTest.java
+++ src/test/java/com/example/contoller/AboutControllerTest.java
@@ -1,11 +1,38 @@
 package com.example.contoller;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser
 class AboutControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @Sql("/sql/About/testAbout.sql")
+    void shouldGetSingleAbout() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/about"))
+                .andDo(print())
+                .andExpect(jsonPath("$.*", hasSize(50)));
+
+
+
+
+    }
 
 }
